f690bd53fe3c23f270c04aac66320c7071c9c93b
sohamsshah <sohamshah456@gmail.com>
2021-04-08 13:37:20 +0530

    integrate disconnect functionality

--- server/server.js
+++ server/server.js
@@ -31,7 +31,25 @@ io.on('connection', (socket)=> {
       socket.on('annotation-data', (data)=> {
             const user = getCurrentUser(socket.id);
             io.to(user.room).emit('annotation-data', data);
-      })
+      });
+
+      socket.on('disconnect', () => {
+            const user = userLeave(socket.id);
+        
+            if (user) {
+              io.to(user.room).emit(
+                'message',
+                 `${user.username} has left the chat`
+              );
+        
+              // Send users and room info
+              io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+              });
+            }
+          });
+
 })
 
 const server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
