5c542d15883d4aed94b688a1f62597332aaa2c76
sohamsshah <sohamshah456@gmail.com>
2021-04-08 12:46:10 +0530

    setup query params

--- client/src/components/Playground/Playground.jsx
+++ client/src/components/Playground/Playground.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import "./Playground.css"
 import Board from "./../Board/Board"
 import io from 'socket.io-client';
+import {useLocation} from "react-router-dom"
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }
 
 function Playground() {
     const socket = io.connect('http://localhost:5000', {transports: ['websocket']});
+    let query = useQuery();
+    console.log(query.get("username"), query.get("room"))
     return (
         <div className="container">
             <div className="color-picker-container">
